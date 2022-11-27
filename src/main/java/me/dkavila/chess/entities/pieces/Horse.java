bf97866987267d15f4a51a6601443d5763c8b819
Derick Avila <derickavila1802@gmail.com>
2022-11-27 11:04:03 -0300

    feat: implement Horse/Knight possible moves

--- src/main/java/me/dkavila/chess/entities/pieces/Horse.java
+++ src/main/java/me/dkavila/chess/entities/pieces/Horse.java
@@ -1,6 +1,7 @@
 package me.dkavila.chess.entities.pieces;
 
 import me.dkavila.board.entities.Board;
+import me.dkavila.board.entities.Position;
 import me.dkavila.chess.entities.ChessPiece;
 import me.dkavila.chess.entities.Color;
 
@@ -17,6 +18,49 @@ public class Horse extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position position = new Position(0, 0);
+
+        // Up moves
+        position.setValues(getPosition().getRow() - 2, getPosition().getColumn() - 1);
+        if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position))){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+        position.setValues(getPosition().getRow() - 2, getPosition().getColumn() + 1);
+        if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position))){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Right moves
+        position.setValues(getPosition().getRow() - 1, getPosition().getColumn()  + 2);
+        if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position))){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+        position.setValues(getPosition().getRow() + 1, getPosition().getColumn() + 2);
+        if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position))){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Left moves
+        position.setValues(getPosition().getRow() + 1, getPosition().getColumn() - 2);
+        if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position))){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+        position.setValues(getPosition().getRow() - 1, getPosition().getColumn() - 2);
+        if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position))){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Down moves
+        position.setValues(getPosition().getRow() + 2, getPosition().getColumn()  - 1);
+        if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position))){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+        position.setValues(getPosition().getRow() + 2, getPosition().getColumn() + 1);
+        if(getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position))){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        return possibleMoves;
     }
 }
