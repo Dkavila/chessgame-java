8db70f44daa1b9a60720424a672f012edac4cd2d
Derick Avila <derickavila1802@gmail.com>
2022-11-27 11:03:15 -0300

    feat: implement Bishop possible moves

--- src/main/java/me/dkavila/chess/entities/pieces/Bishop.java
+++ src/main/java/me/dkavila/chess/entities/pieces/Bishop.java
@@ -1,6 +1,7 @@
 package me.dkavila.chess.entities.pieces;
 
 import me.dkavila.board.entities.Board;
+import me.dkavila.board.entities.Position;
 import me.dkavila.chess.entities.ChessPiece;
 import me.dkavila.chess.entities.Color;
 
@@ -17,6 +18,51 @@ public class Bishop extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position position = new Position(0, 0);
+
+        // Up-right diagonal
+        position.setValues(getPosition().getRow() - 1, getPosition().getColumn() - 1);
+        while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+            position.setValues(position.getRow() - 1, position.getColumn() - 1);
+        }
+        if(getBoard().positionExists(position) && isThereOpponentPiece(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Down-right diagonal
+        position.setValues(getPosition().getRow() - 1, getPosition().getColumn() + 1);
+        while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+            position.setValues(position.getRow() - 1, position.getColumn() + 1);
+        }
+        if(getBoard().positionExists(position) && isThereOpponentPiece(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Down-left diagonal
+        position.setValues(getPosition().getRow() + 1, getPosition().getColumn() - 1);
+        while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+            position.setValues(position.getRow() + 1, position.getColumn() - 1);
+        }
+        if(getBoard().positionExists(position) && isThereOpponentPiece(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Down-right diagonal
+        position.setValues(getPosition().getRow() + 1, getPosition().getColumn() + 1);
+        while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+            position.setValues(position.getRow() + 1, position.getColumn() + 1);
+        }
+        if(getBoard().positionExists(position) && isThereOpponentPiece(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        return possibleMoves;
     }
+
+
 }
