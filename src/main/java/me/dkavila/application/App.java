4175fc0a60970267b9c610b828ad55ce2449e587
Derick Avila <derickavila1802@gmail.com>
2024-04-17 01:11:17 -0300

    clean: remove hello world comments from main file

--- src/main/java/me/dkavila/application/App.java
+++ src/main/java/me/dkavila/application/App.java
@@ -9,10 +9,6 @@ import me.dkavila.chess.exception.ChessException;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
