1bbd29d31bfa6aad92109e068259c0111f08577a
Derick Avila <derickavila1802@gmail.com>
2024-04-18 00:09:07 -0300

    feat: implements King possible moves

--- src/main/java/me/dkavila/chess/entities/pieces/King.java
+++ src/main/java/me/dkavila/chess/entities/pieces/King.java
@@ -1,6 +1,7 @@
 package me.dkavila.chess.entities.pieces;
 
 import me.dkavila.board.entities.Board;
+import me.dkavila.board.entities.Position;
 import me.dkavila.chess.entities.ChessPiece;
 import me.dkavila.chess.entities.Color;
 
@@ -17,6 +18,57 @@ public class King extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] possibleMoves =  new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position position = new Position(0, 0);
+
+        // Above the piece
+        position.setValues(getPosition().getRow() - 1, getPosition().getColumn());
+        if(canMove(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Below the piece
+        position.setValues(getPosition().getRow() + 1, getPosition().getColumn());
+        if(canMove(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Left the piece
+        position.setValues(getPosition().getRow(), getPosition().getColumn() - 1);
+        if(canMove(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Right the piece
+        position.setValues(getPosition().getRow(), getPosition().getColumn() + 1);
+        if(canMove(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Up-Left diagonal
+        position.setValues(getPosition().getRow() - 1, getPosition().getColumn() - 1);
+        if(canMove(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Up-right diagonal
+        position.setValues(getPosition().getRow() - 1, getPosition().getColumn() + 1);
+        if(canMove(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Down-left diagonal
+        position.setValues(getPosition().getRow() + 1, getPosition().getColumn() - 1);
+        if(canMove(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        // Down-right diagonal
+        position.setValues(getPosition().getRow() + 1, getPosition().getColumn() + 1);
+        if(canMove(position)){
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        return possibleMoves;
     }
 }
